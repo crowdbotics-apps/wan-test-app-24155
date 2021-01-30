aac157ef8dd99b896116057d5d6b3bfba77a9f80
Crowdbotics <team@crowdbotics.com>
2021-01-30 10:10:15 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,2 +1,6 @@
 import axios from "axios"
+const test = axios.create({
+  baseURL: "https://www.google.com",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 export const apiService = {}
